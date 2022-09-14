3684b369e8726c20fcc2cde1d86ec50fa9a5707b
Aline Soliman <alineapreis@hotmail.com>
2022-09-14 17:02:27 -0300

    feat: inclui no arquivo app.ts um mmiddleware de erro, importações e rotas necessárias para o endpoint get /products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,40 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import 'express-async-errors';
+import ProductsController from './controllers/products.controller';
 
 const app = express();
 
 app.use(express.json());
+
+const productsController = new ProductsController();
+
+app.get('/products', productsController.getAll);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).send('Express + TypeScript');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
+});
 
 export default app;
